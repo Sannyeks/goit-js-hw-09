d521f49c164172b53b5e1f89000c690f57d22588
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-02-11 15:52:53 +0200

    add new files

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+    myInput : document.getElementById("datetime-picker"),
+    startBtn : document.querySelector('button[data-start]'),
+    days : document.querySelector('[data-days]'),
+    hours : document.querySelector('[data-hours]'),
+    minutes : document.querySelector('[data-minutes]'),
+    seconds : document.querySelector('[data-seconds]'),
+}
+let DATE = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      DATE = selectedDates[0];
+      if(DATE < Date.now()){
+        Notiflix.Notify.warning('Please choose a date in the future');
+        refs.startBtn.setAttribute('disabled','');
+    }
+    else{
+        Notiflix.Notify.success('Click on Start!!!');
+        refs.startBtn.removeAttribute('disabled');
+    }
+  }
+}
+  
+flatpickr(refs.myInput, options);
+
+class Timer {
+    constructor({onTick}) {
+        this.isActive = false;
+        this.intervalId = null;
+        this.onTick = onTick;
+    }
+    start(){
+        if(this.isActive){
+            return;
+        }
+        const startTime = DATE;
+       
+            this.isActive = true;
+            this.intervalId = setInterval(()=> {
+            if(startTime<= Date.now()){
+                clearInterval(this.intervalId);
+                Notiflix.Notify.info('Sale is over!');
+                return;
+    ``       }
+            const currentTime = Date.now();
+            const deltaTime = startTime - currentTime;
+            const time = this.convertMs(deltaTime);
+            this.onTick(time);
+            console.log(this.days.value)
+        },1000);
+    }
+    addLeadingZero(value){
+        return String(value).padStart(2,'0');
+    }
+    convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const days = this.addLeadingZero(Math.floor(ms / day));
+        const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+        const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+        const seconds = this.addLeadingZero(Math.round(Math.floor((((ms % day) % hour) % minute)) / second));
+      
+        return { days, hours, minutes, seconds };
+      }
+}
+const timer = new Timer({
+    onTick : updateClockFace,
+})
+refs.startBtn.addEventListener('click',timer.start.bind(timer))
+
+
+function updateClockFace({days, hours, minutes, seconds}){
+    refs.days.textContent = days; 
+    refs.hours.textContent = hours;  
+    refs.minutes.textContent = minutes;  
+    refs.seconds.textContent = seconds;
+}
+
+document.body.style.backgroundColor = '#E0E0E0';
